3af4271e3882c9f6db973f2e3b5b209d18e0ca10
Hayagreev Dilip <hayagreev.dilip@gmail.com>
2021-10-17 16:39:11 -0700

    Added forward state, and handler method

--- src/main/java/frc/robot/systems/FSMSystem.java
+++ src/main/java/frc/robot/systems/FSMSystem.java
@@ -1,10 +1,11 @@
 package frc.robot.systems;
 
 // WPILib Imports
-import edu.wpi.first.wpilibj.Joystick;
 
 // Third party Hardware Imports
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.ControlType;
 
 // Robot Imports
 import frc.robot.TeleopInput;
@@ -12,19 +13,19 @@ import frc.robot.HardwareMap;
 
 public class FSMSystem {
 	/* ======================== Constants ======================== */
+	public static final double WHEEL_DIAMETER_INCHES = 7.65;
+	public static final double kP = 0.1;
 	// FSM state definitions
 	public enum FSMState {
 		START_STATE,
-		TELEOP_STATE
+		FORWARD_STATE_10_IN,
+		TURN_RIGHT
 	}
 
 	private static final float MOTOR_RUN_POWER = 0.1f;
 
 	/* ======================== Private variables ======================== */
 	private FSMState currentState;
-
-	public static final int WHEEL_PORT = 0;
-	public static final int JOYSTICK_PORT = 1;
 
 	// Hardware devices should be owned by one and only one system. They must
 	// be private to their owner system and may not be used elsewhere.
@@ -35,8 +36,6 @@ public class FSMSystem {
 	private CANSparkMax frontLeftMotor;
 	private CANSparkMax backLeftMotor;
 
-	private Joystick wheel;
-	private Joystick stick;
 
 	/* ======================== Constructor ======================== */
 	/**
@@ -46,17 +45,17 @@ public class FSMSystem {
 	 */
 	public FSMSystem() {
 		// Perform hardware init
-		//exampleMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_SHOOTER,
-										//CANSparkMax.MotorType.kBrushless);
+
 
 		frontRightMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_RIGHT, CANSparkMax.MotorType.kBrushless);
 		frontLeftMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_LEFT, CANSparkMax.MotorType.kBrushless);
 		backRightMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_BACK_RIGHT, CANSparkMax.MotorType.kBrushless);
 		backLeftMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_BACK_LEFT, CANSparkMax.MotorType.kBrushless);
 
-		wheel = new Joystick(WHEEL_PORT);
-		stick = new Joystick(JOYSTICK_PORT);
-		
+		frontRightMotor.getEncoder().setPosition(0);
+		frontLeftMotor.getEncoder().setPosition(0);
+		backRightMotor.getEncoder().setPosition(0);
+		backLeftMotor.getEncoder().setPosition(0);
 		// Reset state machine
 		reset();
 	}
@@ -95,8 +94,8 @@ public class FSMSystem {
 				handleStartState(input);
 				break;
 
-			case TELEOP_STATE:
-				handleTeleOpState(input);
+			case FORWARD_STATE_10_IN:
+				handleForwardState(input, 10, 10);
 				break;
 
 			default:
@@ -119,13 +118,13 @@ public class FSMSystem {
 		switch (currentState) {
 			case START_STATE:
 				if (input != null) {
-					return FSMState.TELEOP_STATE;
+					return FSMState.FORWARD_STATE_10_IN;
 				} else {
 					return FSMState.START_STATE;
 				}
 
-			case TELEOP_STATE:
-				return FSMState.TELEOP_STATE;
+			case FORWARD_STATE_10_IN:
+				return FSMState.TURN_RIGHT;
 
 			default:
 				throw new IllegalStateException("Invalid state: " + currentState.toString());
@@ -139,47 +138,21 @@ public class FSMSystem {
 	 *        the robot is in autonomous mode.
 	 */
 	private void handleStartState(TeleopInput input) {
-        frontRightMotor.set(0);
-        frontLeftMotor.set(0);
-        backRightMotor.set(0);
-        backLeftMotor.set(0);
-    }
-	/**
-	 * Handle behavior in OTHER_STATE.
-	 * @param input Global TeleopInput if robot in teleop mode or null if
-	 *        the robot is in autonomous mode.
-	 */
-	private void handleTeleOpState(TeleopInput input) {
-		double leftPower = -stick.getY() * (1 + wheel.getDirectionDegrees() / 90);
-		double rightPower = stick.getY() * (1 - wheel.getDirectionDegrees() / 90);
-		
-		// double scalar;
-
-		// if(Math.abs(leftPower) > 1){
-		// 	if(Math.abs(leftPower) > Math.abs(rightPower)){
-		// 		scalar = Math.abs(1 / leftPower);
-		// 		leftPower *= scalar;
-		// 		rightPower *= scalar;
-		// 	}
-		// }else if(Math.abs(rightPower) > 1){
-		// 	if(Math.abs(leftPower) < Math.abs(rightPower)){
-		// 		scalar = Math.abs(1 / rightPower);
-		// 		leftPower *= scalar;
-		// 		rightPower *= scalar;
-		// 	}
-		// }
-
-		limitPower(leftPower);
-		limitPower(rightPower);
-
-		frontRightMotor.set(rightPower);
-        frontLeftMotor.set(leftPower);
-        backRightMotor.set(rightPower);
-        backLeftMotor.set(leftPower);
+		frontRightMotor.set(0);
+		frontLeftMotor.set(0);
+		backRightMotor.set(0);
+		backLeftMotor.set(0);
 	}
 
-	private void limitPower(double number){
-		if(number > 1) number = 1;
-		if(number < -1) number = -1;
+	//Assume power for both left and right are 10
+	public void handleForwardState(TeleopInput input, double inches) {
+		double currentPos_inches = frontLeftMotor.getEncoder().getPosition() * Math.PI * WHEEL_DIAMETER_INCHES;
+		double error = currentPos_inches - inches;
+		double speed = kP * error;
+
+		frontLeftMotor.set(speed);
+		frontRightMotor.set(speed);
+		backLeftMotor.set(speed);
+		backRightMotor.set(speed);
 	}
 }
