6fad4a75df2847392fa6e201c023e9585419f79f
RahulBansal <rahulbansal2004@gmail.com>
2021-11-03 09:12:10 -0700

    merged and fixed errors

--- src/main/java/frc/robot/systems/FSMSystem.java
+++ src/main/java/frc/robot/systems/FSMSystem.java
@@ -4,10 +4,6 @@ package frc.robot.systems;
 
 // Third party Hardware Imports
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-import com.revrobotics.ControlType;
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SPI;
 
 // Robot Imports
 import frc.robot.TeleopInput;
@@ -15,14 +11,10 @@ import frc.robot.HardwareMap;
 
 public class FSMSystem {
 	/* ======================== Constants ======================== */
-	public static final double WHEEL_DIAMETER_INCHES = 7.65;
-	public static final double kP_move_straight = 0.1;
 	// FSM state definitions
 	public enum FSMState {
 		START_STATE,
-		FORWARD_STATE_10_IN,
-		TURN_STATE,
-		TELEOP_STATE
+		OTHER_STATE
 	}
 
 	private static final float MOTOR_RUN_POWER = 0.1f;
@@ -32,12 +24,7 @@ public class FSMSystem {
 
 	// Hardware devices should be owned by one and only one system. They must
 	// be private to their owner system and may not be used elsewhere.
-	//private CANSparkMax exampleMotor;
-
-	private CANSparkMax frontRightMotor;
-	private CANSparkMax backRightMotor;
-	private CANSparkMax frontLeftMotor;
-	private CANSparkMax backLeftMotor;
+	private CANSparkMax exampleMotor;
 
 	/* ======================== Constructor ======================== */
 	/**
@@ -47,17 +34,9 @@ public class FSMSystem {
 	 */
 	public FSMSystem() {
 		// Perform hardware init
+		exampleMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_SHOOTER,
+										CANSparkMax.MotorType.kBrushless);
 
-		frontRightMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_RIGHT, CANSparkMax.MotorType.kBrushless);
-		frontLeftMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_FRONT_LEFT, CANSparkMax.MotorType.kBrushless);
-		backRightMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_BACK_RIGHT, CANSparkMax.MotorType.kBrushless);
-		backLeftMotor = new CANSparkMax(HardwareMap.CAN_ID_SPARK_DRIVE_BACK_LEFT, CANSparkMax.MotorType.kBrushless);
-
-		
-		frontRightMotor.getEncoder().setPosition(0);
-		frontLeftMotor.getEncoder().setPosition(0);
-		backRightMotor.getEncoder().setPosition(0);
-		backLeftMotor.getEncoder().setPosition(0);
 		// Reset state machine
 		reset();
 	}
@@ -96,16 +75,8 @@ public class FSMSystem {
 				handleStartState(input);
 				break;
 
-			case TELEOP_STATE:
-				handleTeleOpState(input);
-                break;
-                
-			case FORWARD_STATE_10_IN:
-				handleForwardOrBackwardState(input, 10, 10);
-				break;
-
-			case TURN_STATE:
-				handleTurnState(input, 90); // test 90 degrees
+			case OTHER_STATE:
+				handleOtherState(input);
 				break;
 
 			default:
@@ -128,19 +99,13 @@ public class FSMSystem {
 		switch (currentState) {
 			case START_STATE:
 				if (input != null) {
-					return FSMState.TELEOP_STATE;
+					return FSMState.OTHER_STATE;
 				} else {
 					return FSMState.START_STATE;
 				}
 
-			case TELEOP_STATE:
-				return FSMState.TELEOP_STATE;
-                
-			case FORWARD_STATE_10_IN:
-				return FSMState.TURN_RIGHT;
-
-			case TURN_STATE:
-				return FSMState.START_STATE;
+			case OTHER_STATE:
+				return FSMState.OTHER_STATE;
 
 			default:
 				throw new IllegalStateException("Invalid state: " + currentState.toString());
@@ -154,86 +119,14 @@ public class FSMSystem {
 	 *        the robot is in autonomous mode.
 	 */
 	private void handleStartState(TeleopInput input) {
-		setPowerForAllMotors(0);
+		exampleMotor.set(0);
 	}
-
-	//Assume encoder starts at 0
 	/**
-	* Handle behavior in FORWARD_STATE, or BACKWARD_STATE
-	* @param input Global TeleopInput if robot in teleop mode or null if
-	*        the robot is in autonomous mode.
-	* @param inches The number of inches to move forward or backward
-	*/
-	private void handleForwardOrBackwardState(TeleopInput input, double inches) {
-		// if(Math.abs(leftPower) > 1){
-		// 	if(Math.abs(leftPower) > Math.abs(rightPower)){
-		// 		scalar = Math.abs(1 / leftPower);
-		// 		leftPower *= scalar;
-		// 		rightPower *= scalar;
-		// 	}
-		// }else if(Math.abs(rightPower) > 1){
-		// 	if(Math.abs(leftPower) < Math.abs(rightPower)){
-		// 		scalar = Math.abs(1 / rightPower);
-		// 		leftPower *= scalar;
-		// 		rightPower *= scalar;
-		// 	}
-		// }
-
-		limitPower(leftPower);
-		limitPower(rightPower);
-
-		frontRightMotor.set(rightPower);
-        frontLeftMotor.set(leftPower);
-        backRightMotor.set(rightPower);
-        backLeftMotor.set(leftPower);
+	 * Handle behavior in OTHER_STATE.
+	 * @param input Global TeleopInput if robot in teleop mode or null if
+	 *        the robot is in autonomous mode.
+	 */
+	private void handleOtherState(TeleopInput input) {
+		exampleMotor.set(MOTOR_RUN_POWER);
     }
-    
-	//Assume encoder positions are at 0 initially
-	public void handleForwardOrBackwardState(TeleopInput input, double inches) {
-		double currentPos_inches = frontLeftMotor.getEncoder().getPosition() * Math.PI * WHEEL_DIAMETER_INCHES;
-		double error = inches - currentPos_inches;
-		double speed = kP_move_straight * error;
-
-		if(speed >= 1) {
-			setPowerForAllMotors(1);
-		} else if(speed <= -1) {
-			setPowerForAllMotors(-1);
-		} else {
-			setPowerForAllMotors(speed);
-		}
-	}
-	
-	/**
-	* Sets power for all motors
-	* @param power The power to set all the motors to
-	*/
-	public void setPowerForAllMotors(double power) {
-		frontLeftMotor.set(power);
-		frontRightMotor.set(power);
-		backLeftMotor.set(power);
-		backRightMotor.set(power);
-	}
-
-	public void handleTurnState(TeleopInput input, double degrees) { // turn x degrees, +x is right, -x is left
-		double error = degrees - getHeading();
-		double power = Math.abs(error / 360) * (error < 0 ? -1 : 1)
-
-		frontLeftMotor.set(power);
-		frontRightMotor.set(power);
-		backLeftMotor.set(power;
-		backRightMotor.set(power);
-	}
-
-	public double getHeading() {
-		return -Math.IEEEremainder(gyro.getAngle(), 360);
-	}
-
-	private void limitPower(double number){
-		if(number > 1) {
-			number = 1;
-		}
-		if(number < -1) {
-			number = -1;
-		}
-	}
 }
